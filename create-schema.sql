a1623a684c67fdb9249b344cb96fc88082690510
Emil Ahlberg <emil.ahlberg1@gmail.com>
2019-04-03 11:41:12 +0200

    Reset scriptfile v1.0

--- create-schema.sql
+++ create-schema.sql
@@ -1,26 +1,30 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes(
     bar_code    INT,
     name        TEXT,
     PRIMARY KEY (bar_code)
 );
 
+DROP TABLE IF EXISTS customers;
 CREATE TABLE customers(
     customer_name   TEXT,
     address         TEXT,
     PRIMARY KEY (customer_name)
 );
 
+DROP TABLE IF EXISTS raw_materials;
 CREATE TABLE raw_materials(
     ingredient_name TEXT,
     balance         INT,
     unit            TEXT,
     update_date     DATE,
     update_amount   INT,
-    PRIMARY_KEY (ingredient_name)
+    PRIMARY KEY (ingredient_name)
 );
 
+DROP TABLE IF EXISTS recipe_entries;
 CREATE TABLE recipe_entries(
     amount          INT,
     bar_code        INT,
@@ -30,6 +34,7 @@ CREATE TABLE recipe_entries(
     FOREIGN KEY (ingredient_name) REFERENCES raw_materials(ingredient_name)
 );
 
+DROP TABLE IF EXISTS orders;
 CREATE TABLE orders(
     order_id        INT,
     order_date      DATE,
@@ -39,17 +44,20 @@ CREATE TABLE orders(
     FOREIGN KEY (customer_name) REFERENCES customers(customer_name)
 );
 
+DROP TABLE IF EXISTS pallets;
 CREATE TABLE pallets(
     pallet_nbr  INT,
+    bar_code    INT,
     time        TIME,
     date        DATE,
     is_blocked  INT,
     order_id    INT,
     PRIMARY KEY (pallet_nbr),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (bar_code)  REFERENCES recipes(bar_code)
 );
 
-
+DROP TABLE IF EXISTS order_spec;
 CREATE TABLE order_spec(
     bar_code    INT,
     order_id    INT,
--- rest.py
+++ rest.py
@@ -21,7 +21,8 @@ def resetDatabase():
     data = ""
     with open('resetAndInit.txt', 'r') as myfile:
         data=myfile.read()
-    c.executescript(sqlScript)
+    print(data)
+    c.executescript(data)
 
 @get('/customers')
 def pong():
@@ -89,20 +90,37 @@ def recipes():
 
 @post('/pallets')
 def pallets():
-        queryDict = request.query
-        c = conn.cursor()
-        #   needed?
-        #   c.execute('PRAGMA foreign_keys=ON')
-        try:
-            for row in c.execute(
-                """
-                INSERT INTO tickets (performanceid, name, ticketid)
-                VALUES 	(?, ?, ?)
-                """, [queryDict.get('user'), queryDict.get('pwd'), queryDict.get('performanceid')]
-            ):
-            return("/tickets/%s" % id)
-        except sqlite3.IntegrityError:
-            return ("Error")
+    queryDict = request.query
+    c = conn.cursor()
+    c.execute('PRAGMA foreign_keys=ON') #needed?
+
+    # 15 cookies/bag, 10 bags/box,  36 boxes/pallet
+
+    for row in c.execute(
+        """
+        SELECT  ingredient_name, amount*15*10*36 > balance AS inStock
+        FROM    recipes
+        JOIN    recipe_entries
+        USING   (bar_code)
+        JOIN    raw_materials
+        USING   (ingredient_name)
+        WHERE   name=?
+        """, [queryDict.get('cookie')]
+    ):
+
+        print(row)
+
+    try:
+        # for row in c.execute(
+        #     """
+        #
+        #     INSERT INTO pallets (performanceid, name, ticketid)
+        #     VALUES 	(?, ?, ?)
+        #     """, [queryDict.get('cookie')]
+        # )
+        return ("/pallets/%s"   % id)
+    except sqlite3.IntegrityError:
+        return ("Error")
 
 
 run(host='localhost', port=8888, debug = True)
